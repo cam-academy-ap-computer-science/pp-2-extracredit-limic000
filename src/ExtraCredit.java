205bfb3f428abbcea91deee386033e124c062689
li_mic000 <li_mic000@CAM-LAB-09.bgsd.k12.wa.us>
2019-10-18 07:56:56 -0700

    Updated 10/18/2019

--- src/ExtraCredit.java
+++ src/ExtraCredit.java
@@ -5,7 +5,7 @@ make a needle
 		spaces on either side = SIZE + 4
 	make another loop for ||
 		make one || for each line
-make the top part of the top
+make the repeated part
 	outside of loop do one __/
 	then in the loop make amount of lines * 3 worth of :
 	outside of loop do ||
@@ -15,18 +15,29 @@ make the top part of the top
 		make a line |
 		then make a loop that does 3 * SIZE worth of "
 		make a line |
-
+make the bottom of th top
+	outside of loop do \_
+	then loop and print size*3 - 1 wort of /\
+	outside of loop do _/
+call the needle part again
+then make the tower
+	|%%||%%| for 4*size
+call repeated part again
 
  */
 
 public class ExtraCredit {
 	public static final int SIZE = 4;
 	public static void main(String[]args) {
-		
+		needle();
 	}
 	public static void needle() {
-		for (int i = 1; i < SIZE; i++) {
+		for (int lines = 1; lines <= SIZE; lines++) {
+			for (int space = 1; space <= 3*SIZE; space++) {
+				System.out.print(" ");
+			}	
 			System.out.println("||");
+		
 		}
 	}
 }
